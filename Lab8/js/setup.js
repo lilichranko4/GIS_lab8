7f56b3fc23eaa367af54cce3e3a82a25d7c0d966
lilichranko4 <Tuo53044@Temple.edu>
2024-11-15 17:11:42 -0500

    Delete Lab8 directory

--- Lab8/js/setup.js
+++
@@ -1,8 +0,0 @@
-var map = L.map('map', {
-  center: [44.47802170412041, -73.19722678261232],
-  zoom: 17,
-});
-
-var Satellite = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
